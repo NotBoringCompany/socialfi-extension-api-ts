2acd989d445cb5c716fabd1f803dc3abf7ba45c2
suwandre <suwandresukijat@gmail.com>
2024-02-03 22:51:42 +0100

    chore: express session init

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import * as dotenv from 'dotenv';
 import passport from 'passport';
 import mongoose from 'mongoose';
+import session from 'express-session';
 
 dotenv.config();
 
@@ -15,11 +16,19 @@ app.use(express.json());
 // temporarily allowing all cors requests
 app.use(cors());
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: mongoUri })
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 /** ROUTE IMPORTS */
 import twitterAuth from './routes/auth/twitter';
+import MongoStore from 'connect-mongo';
 
 app.use('/auth/twitter', twitterAuth);
 
