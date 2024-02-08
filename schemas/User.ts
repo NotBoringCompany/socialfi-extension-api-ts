7785afac5d4e82c477d104e2940bada290947fc7
suwandre <suwandresukijat@gmail.com>
2024-02-08 18:34:28 +0100

    rf: refactor user schema

--- schemas/User.ts
+++ schemas/User.ts
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
 
+/**
+ * User schema. Represents closely to the `User` interface in `models/user.ts`.
+ */
 export const UserSchema = new mongoose.Schema({
     _id: {
         type: String,
         default: new mongoose.Types.ObjectId()
     },
-    // when logged in via twitter, the user's twitterId is stored
-    twitterId: String
+    twitterId: String,
+    wallet: Object,
+    openedTweetIdsToday: Array,
+    inventory: Object
 })
