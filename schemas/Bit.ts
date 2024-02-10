bb13c3baade9cc5900196cad15850e21b6997209
suwandre <suwandresukijat@gmail.com>
2024-02-10 11:23:42 +0100

    feat: bit schema

---
+++ schemas/Bit.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+/**
+ * Bit schema. Represents closely to the `Bit` interface in `models/bit.ts`.
+ */
+export const BitSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        default: new mongoose.Types.ObjectId()
+    },
+    bitId: Number,
+    rarity: String,
+    gender: String,
+    owner: String,
+    purchaseDate: Number,
+    obtainMethod: String,
+    totalCookiesSpent: Number,
+    currentFarmingLevel: Number,
+    farmingStats: Object,
+    bitStatsModifiers: Object
+})
