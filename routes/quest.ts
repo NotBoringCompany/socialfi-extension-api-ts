ef4163faa109b5c40f98ad9437fd92ce98d6e0e0
suwandre <suwandresukijat@gmail.com>
2024-02-20 11:36:17 +0100

    feat: quest routes

--- routes/quest.ts
+++ routes/quest.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addQuest } from '../api/quest';
+import { addQuest, completeQuest, deleteQuest, getQuests, getUserCompletedQuests } from '../api/quest';
 
 const router = express.Router();
 
@@ -35,6 +35,82 @@ router.post('/add_quest', async (req, res) => {
             data
         });
     
+    } catch (err: any) {
+        return res.status(500).json({
+            status: 500,
+            message: err.message
+        })
+    }
+});
+
+// temporarily without authentication for testing purposes
+router.post('/complete_quest', async (req, res) => {
+    const { twitterId, questId } = req.body;
+
+    try {
+        const { status, message, data } = await completeQuest(twitterId, questId);
+
+        return res.status(status).json({
+            status,
+            message,
+            data
+        });
+    } catch (err: any) {
+        return res.status(500).json({
+            status: 500,
+            message: err.message
+        })
+    }
+});
+
+router.get('/get_quests', async (_, res) => {
+    try {
+        const { status, message, data } = await getQuests();
+
+        return res.status(status).json({
+            status,
+            message,
+            data
+        });
+    } catch (err: any) {
+        return res.status(500).json({
+            status: 500,
+            message: err.message
+        })
+    }
+});
+
+router.post('/delete_quest', async (req, res) => {
+    const { questId, adminKey } = req.body;
+
+    try {
+        const { status, message, data } = await deleteQuest(questId, adminKey);
+
+        return res.status(status).json({
+            status,
+            message,
+            data
+        });
+    } catch (err: any) {
+        return res.status(500).json({
+            status: 500,
+            message: err.message
+        })
+    }
+});
+
+// temporarily without authentication for testing purposes
+router.get('/get_user_completed_quests', async (req, res) => {
+    const { twitterId } = req.body;
+
+    try {
+        const { status, message, data } = await getUserCompletedQuests(twitterId);
+
+        return res.status(status).json({
+            status,
+            message,
+            data
+        });
     } catch (err: any) {
         return res.status(500).json({
             status: 500,
