38a84e645eebf89755fd2a6ec1e40e57528d2e35
suwandre <suwandresukijat@gmail.com>
2024-02-21 12:54:01 +0100

    rf: complete_quest route to use jwt token

--- routes/quest.ts
+++ routes/quest.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { addQuest, completeQuest, deleteQuest, getQuests, getUserCompletedQuests } from '../api/quest';
+import { validateJWT } from '../utils/jwt';
+import { Status } from '../utils/retVal';
 
 const router = express.Router();
 
@@ -43,9 +45,39 @@ router.post('/add_quest', async (req, res) => {
     }
 });
 
-// temporarily without authentication for testing purposes
 router.post('/complete_quest', async (req, res) => {
-    const { twitterId, questId } = req.body;
+    // allow from twitter.com
+    res.header('Access-Control-Allow-Origin', 'https://twitter.com');
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    const { questId } = req.body;
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1]; // bearer JWT token
+
+    if (!token) {
+        return res.status(401).json({
+            status: Status.UNAUTHORIZED,
+            message: '(complete_quest) No token provided.',
+        });
+    }
+
+    const { status: validateStatus, message: validateMessage, data: validateData } = validateJWT(token);
+    if (validateStatus !== Status.SUCCESS) {
+        return res.status(validateStatus).json({
+            validateStatus,
+            validateMessage
+        });
+    }
+
+    const { twitterId } = validateData;
+
+    if (!twitterId) {
+        return res.status(401).json({
+            status: Status.UNAUTHORIZED,
+            message: '(complete_quest) You denied the app or your session has expired.'
+        });
+    }
 
     try {
         const { status, message, data } = await completeQuest(twitterId, questId);
