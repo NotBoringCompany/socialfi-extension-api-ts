fe4cf9e63e48bb133478df1e55b603d81b91d08f
suwandre <suwandresukijat@gmail.com>
2024-03-28 19:58:31 +0100

    feat: added travel_to_city route

--- routes/city.ts
+++ routes/city.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { validateRequestAuth } from '../utils/auth';
 import { Status } from '../utils/retVal';
-import { addCity } from '../api/city';
+import { addCity, travelToCity } from '../api/city';
 
 const router = express.Router();
 
@@ -19,6 +19,37 @@ router.post('/add_city', async (req, res) => {
         }
 
         const { status, message } = await addCity(name, distanceTo, shop, adminKey);
+
+        return res.status(status).json({
+            status,
+            message
+        });
+    } catch (err: any) {
+        return res.status(500).json({
+            status: 500,
+            message: err.message
+        })
+    }
+})
+
+router.post('/travel_to_city', async (req, res) => {
+    const { destination } = req.body;
+
+    try {
+        const {
+            status: validateStatus,
+            message: validateMessage,
+            data: validateData,
+        } = await validateRequestAuth(req, res, 'travel_to_city');
+
+        if (validateStatus !== Status.SUCCESS) {
+            return res.status(validateStatus).json({
+                status: validateStatus,
+                message: validateMessage,
+            });
+        }
+
+        const { status, message } = await travelToCity(validateData?.twitterId, destination);
 
         return res.status(status).json({
             status,
