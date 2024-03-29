fc3684fde6c25c28292b123aaaf9f2c80cfa9b49
suwandre <suwandresukijat@gmail.com>
2024-03-29 12:04:05 +0100

    feat: created leaderboard model

---
+++ models/leaderboard.ts
@@ -0,0 +1,31 @@
+/**
+ * Represents a leaderboard.
+ */
+export interface Leaderboard {
+    /** the name of the leaderboard */
+    name: string;
+    /** the type of leaderboard */
+    type: LeaderboardType;
+    /** user data for this leaderboard (points etc)  */
+    userData: LeaderboardUserData[];
+}
+
+/**
+ * Lists the types of leaderboards.
+ */
+export enum LeaderboardType {
+    /** main leaderboards are standalone leaderboards */
+    MAIN = 'Main',
+    /** weekly leaderboards may get reset and/or get added to main leaderboards */
+    WEEKLY = 'Weekly'
+}
+
+/**
+ * Represents user data for a leaderboard.
+ */
+export interface LeaderboardUserData {
+    /** the user's database ID */
+    userId: string;
+    /** the user's points */
+    points: number;
+}
