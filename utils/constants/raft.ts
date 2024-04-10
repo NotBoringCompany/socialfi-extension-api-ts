08add44aa767b5cd360ed13cd0e5b60c912af49f
Jason Sujaya <sujaya.jason@gmail.com>
2024-04-10 19:51:49 +0700

    Adjusted formula for raft speed and also add the raft capacity

--- utils/constants/raft.ts
+++ utils/constants/raft.ts
@@ -2,20 +2,30 @@
  * Randomizes the raft's base speed (+- 10% of 10 m/s)
  */
 export const randomizeRaftBaseSpeed = () => {
-    return 10 + Math.random() * 2 - 1;
-}
+  return 10 + Math.random() * 2 - 1;
+};
 
 /**
  * Gets the actual raft speed based on its level.
  */
 export const ACTUAL_RAFT_SPEED = (baseSpeed: number, currentLevel: number) => {
-    return baseSpeed + (0.1 * currentLevel - 1);
-}
+  return baseSpeed + (1 * currentLevel - 1);
+};
+
+/**
+ * Gets the actual raft capacity based on its level.
+ */
+export const ACTUAL_RAFT_CAPACITY = (
+  baseCapacity: number,
+  currentLevel: number
+) => {
+  return baseCapacity + (10 * currentLevel - 1);
+};
 
 /**
  * Calculates the cost (in xCookies) of evolving the raft given the current level of the raft.
  */
 export const RAFT_EVOLUTION_COST = (currentLevel: number) => {
-    // price starts at 600 cookies at currentLevel of 1 and 100 cookies increase per level
-    return 500 + (100 * currentLevel);
-}
+  // price starts at 600 cookies at currentLevel of 1 and 100 cookies increase per level
+  return 500 + 100 * currentLevel;
+};
