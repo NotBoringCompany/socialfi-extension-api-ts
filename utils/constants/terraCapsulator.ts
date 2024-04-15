86f440231b8cca0df9f837eac9f1cc2196e0f8f6
Jason Sujaya <sujaya.jason@gmail.com>
2024-04-15 20:19:34 +0700

    Added the terra capsulator II chance

--- utils/constants/terraCapsulator.ts
+++ utils/constants/terraCapsulator.ts
@@ -1,17 +1,50 @@
 import { IslandType } from '../../models/island';
 
+// /**
+//  * Gets the Island's type based on the probability of obtaining it, depending on `rand`, which is a number from 1 to 100
+//  */
+// export const randomizeTypeFromCapsulator = (): IslandType => {
+//     const rand = Math.floor(Math.random() * 100) + 1;
+
+//     switch (true) {
+//         case rand < 76:
+//             return IslandType.PRIMAL_ISLES; // 75% chance
+//         case rand < 96:
+//             return IslandType.VERDANT_ISLES; // 20% chance
+//         default:
+//             return IslandType.EXOTIC_ISLES; // 5% chance
+//     }
+// }
+
 /**
  * Gets the Island's type based on the probability of obtaining it, depending on `rand`, which is a number from 1 to 100
  */
-export const randomizeTypeFromCapsulator = (): IslandType => {
-    const rand = Math.floor(Math.random() * 100) + 1;
+export const randomizeTypeFromCapsulator = (
+  capsuleType: string = ""
+): IslandType => {
+  const rand = Math.floor(Math.random() * 100) + 1;
 
-    switch (true) {
-        case rand < 76:
-            return IslandType.PRIMAL_ISLES; // 75% chance
-        case rand < 96:
-            return IslandType.VERDANT_ISLES; // 20% chance
-        default:
-            return IslandType.EXOTIC_ISLES; // 5% chance
-    }
-}
+  switch (capsuleType) {
+    case "Terra Capsule":
+      return rand < 31
+        ? IslandType.PRIMAL_ISLES
+        : rand < 81
+        ? IslandType.VERDANT_ISLES
+        : rand < 96
+        ? IslandType.EXOTIC_ISLES
+        : IslandType.CRYSTAL_ISLES;
+    case "Terra Capsule (II)":
+      return rand < 31
+        ? IslandType.PRIMAL_ISLES
+        : rand < 81
+        ? IslandType.VERDANT_ISLES
+        : rand < 96
+        ? IslandType.EXOTIC_ISLES
+        : rand < 101
+        ? IslandType.CRYSTAL_ISLES
+        : IslandType.CRYSTAL_ISLES;
+    default:
+      return IslandType.PRIMAL_ISLES; // Default to Primal Isles when no or empty capsule type is provided
+  }
+};
+
