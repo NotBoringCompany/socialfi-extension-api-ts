3292fc4a8e9b9a4daedef41cd976ebae9e3eed5e
suwandre <suwandresukijat@gmail.com>
2024-05-06 10:58:40 +0200

    feat: squad schema

---
+++ schemas/Squad.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import { generateObjectId } from '../utils/crypto';
+
+/**
+ * Squad schema. Represents closely to the `Squad` interface in `models/squad.ts`.
+ */
+export const SquadSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        default: generateObjectId()
+    },
+    name: String,
+    members: [{
+        userId: String,
+        role: String,
+        joinedTimestamp: Number,
+        roleUpdatedTimestamp: Number
+    }],
+    formedTimestamp: Number
+})
