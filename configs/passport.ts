f95f6e979abddc8d37ab0a4b9f06383798c2474d
suwandre <suwandresukijat@gmail.com>
2024-05-14 11:14:08 +0200

    rf: fixed typo in discord strategy

--- configs/passport.ts
+++ configs/passport.ts
@@ -29,8 +29,8 @@ passport.use(new TwitterStrategy({
 
 passport.use(new DiscordStrategy({
     clientID: process.env.DISCORD_CLIENT_ID,
-    clientSecret: process.env.DISCORD_CLIENT_SECRET ,
-    callbackURL: process.env.DISCORD_CALLBACK_URL
+    clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    callbackURL: process.env.DISCORD_CALLBACK_URL,
     scope: ['identify', 'role_connections.write']
 }, (accessToken, refreshToken, profile, done) => {
     try {
