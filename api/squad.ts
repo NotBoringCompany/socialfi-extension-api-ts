b46c8820c57899ecd13bc57d42786ee9664fa292
suwandre <suwandresukijat@gmail.com>
2024-05-17 12:12:04 +0200

    rf: uncommented leaveSquad

--- api/squad.ts
+++ api/squad.ts
@@ -741,19 +741,19 @@ export const leaveSquad = async (twitterId: string): Promise<ReturnValue> => {
                     },
                 }).then(data => console.log('data from updating here: ', data)).catch(err => console.log('error from updating here: ', err));
 
-                // await SquadModel.updateOne({ _id: squad._id }, {
-                //     $pull: {
-                //         members: {
-                //             userId: user._id
-                //         }
-                //     }
-                // });
+                await SquadModel.updateOne({ _id: squad._id }, {
+                    $pull: {
+                        members: {
+                            userId: user._id
+                        }
+                    }
+                });
 
-                // // update the user's squad ID and `lastLeftSquad` timestamp.
-                // await UserModel.updateOne({ _id: user._id }, {
-                //     'inGameData.squadId': null,
-                //     'inGameData.lastLeftSquad': Math.floor(Date.now() / 1000)
-                // });
+                // update the user's squad ID and `lastLeftSquad` timestamp.
+                await UserModel.updateOne({ _id: user._id }, {
+                    'inGameData.squadId': null,
+                    'inGameData.lastLeftSquad': Math.floor(Date.now() / 1000)
+                });
 
                 return {
                     status: Status.SUCCESS,
