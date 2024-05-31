df0135922c3d5f600f8fa8d2d525c13de573c2de
suwandre <suwandresukijat@gmail.com>
2024-05-31 14:14:38 +0200

    feat: added kos claimable daily and weekly reward models

--- models/kos.ts
+++ models/kos.ts
@@ -1,3 +1,7 @@
+import { BitOrbType } from './bitOrb';
+import { BoosterItem } from './booster';
+import { TerraCapsulatorType } from './terraCapsulator';
+
 /**
  * Represents the full metadata of a Key Of Salvation.
  */
@@ -72,4 +76,58 @@ export enum KOSAuraTypes {
     SNOW = 'Snow',
     LIGHTNING = 'Lightning',
     SMOKE = 'Smoke',
+}
+
+/**
+ * Represents the claimable daily rewards for any eligible user for KOS benefits.
+ */
+export interface KOSClaimableDailyReward {
+    /** the user's database ID */
+    userId: string;
+    /** the user's username */
+    username: string;
+    /** the user's twitter profile picture URL */
+    twitterProfilePicture: string;
+    /** the claimable daily rewards the user can claim */
+    claimableRewards: KOSReward[];
+}
+
+/**
+ * Represents the claimable weekly rewards for any eligible user for KOS benefits.
+ */
+export interface KOSClaimableWeeklyReward {
+    /** the user's database ID */
+    userId: string;
+    /** the user's username */
+    username: string;
+    /** the user's twitter profile picture URL */
+    twitterProfilePicture: string;
+    /** the claimable weekly rewards the user can claim */
+    claimableRewards: KOSReward[];
+}
+
+/**
+ * Represents a KOS reward instance.
+ */
+export interface KOSReward {
+    /** the reward type */
+    type: KOSRewardType;
+    /** the amount of the reward to give */
+    amount: number;
+}
+
+/**
+ * Represents all available KOS reward types.
+ */
+export enum KOSRewardType {
+    X_COOKIES = 'xCookies',
+    GATHERING_PROGRESS_BOOSTER_25 = BoosterItem.GATHERING_PROGRESS_BOOSTER_25,
+    GATHERING_PROGRESS_BOOSTER_50 = BoosterItem.GATHERING_PROGRESS_BOOSTER_50,
+    GATHERING_PROGRESS_BOOSTER_100 = BoosterItem.GATHERING_PROGRESS_BOOSTER_100,
+    LEADERBOARD_POINTS = 'Leaderboard Points',
+    BIT_ORB_I = BitOrbType.BIT_ORB_I,
+    BIT_ORB_II = BitOrbType.BIT_ORB_II,
+    TERRA_CAPSULATOR_I = TerraCapsulatorType.TERRA_CAPSULATOR_I,
+    TERRA_CAPSULATOR_II = TerraCapsulatorType.TERRA_CAPSULATOR_II,
+    RAFT_SPEED_BOOSTER_60_MIN = BoosterItem.RAFT_SPEED_BOOSTER_60_MIN,
 }
