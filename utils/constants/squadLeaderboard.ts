67a935099edfd04046789630612461787601ef22
suwandre <suwandresukijat@gmail.com>
2024-06-12 21:58:49 +0200

    rf: updated squad weekly ranking

--- utils/constants/squadLeaderboard.ts
+++ utils/constants/squadLeaderboard.ts
@@ -4,20 +4,19 @@ import { SquadRank } from '../../models/squad'
  * Gets a squad's new weekly ranking based on the points earned the previous week.
  */
 export const GET_SQUAD_WEEKLY_RANKING = (points: number): SquadRank => {
-    if (points >= 1 && points <= 4999) {
-        return SquadRank.BRONZE;
-    } else if (points >= 5000 && points <= 9999) {
-        return SquadRank.SILVER;
-    } else if (points >= 10000 && points <= 14999) {
-        return SquadRank.GOLD;
-    } else if (points >= 15000 && points <= 19999) {
-        return SquadRank.PLATINUM;
-    } else if (points >= 20000 && points <= 24999) {
-        return SquadRank.DIAMOND;
-    } else if (points >= 25000) {
-        return SquadRank.MASTER;
-    // if the squad has no points, they are unranked
-    } else {
+    if (points >= 1 && points <= 99999) {
         return SquadRank.UNRANKED;
+    } else if (points >= 100000 && points <= 999999) {
+        return SquadRank.BRONZE;
+    } else if (points >= 1000000 && points <= 4999999) {
+        return SquadRank.SILVER;
+    } else if (points >= 5000000 && points <= 19999999) {
+        return SquadRank.GOLD;
+    } else if (points >= 20000000 && points <= 49999999) {
+        return SquadRank.PLATINUM;
+    } else if (points >= 50000000 && points <= 99999999) {
+        return SquadRank.DIAMOND;
+    } else {
+        return SquadRank.MASTER;
     }
 }
