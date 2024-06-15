be9a1541d7a2a2ad98f01b6ec658007629c29814
suwandre <suwandresukijat@gmail.com>
2024-06-15 11:59:15 +0200

    rf: updated getWeeklyMVPContenders to check if xCookieData is null

--- api/weeklyMVPReward.ts
+++ api/weeklyMVPReward.ts
@@ -40,7 +40,7 @@ export const getWeeklyMVPContenders = async (): Promise<ReturnValue> => {
         }[] = [];
 
         for (const user of users) {
-            const xCookiesSpent = (user.inventory.xCookieData as XCookieData).weeklyXCookiesSpent ?? 0;
+            const xCookiesSpent = (user.inventory.xCookieData as XCookieData)?.weeklyXCookiesSpent ?? 0;
 
             const bitOrbsConsumed = (user.inventory.items as Item[]).reduce((acc, item) => {
                 if (item.type === BitOrbType.BIT_ORB_I || item.type === BitOrbType.BIT_ORB_II || item.type === BitOrbType.BIT_ORB_III) {
@@ -134,7 +134,7 @@ export const distributeWeeklyMVPRewards = async (): Promise<void> => {
         }[] = [];
 
         for (const user of users) {
-            const xCookiesSpent = (user.inventory.xCookieData as XCookieData).weeklyXCookiesSpent ?? 0;
+            const xCookiesSpent = (user.inventory.xCookieData as XCookieData)?.weeklyXCookiesSpent ?? 0;
 
             const bitOrbsConsumed = (user.inventory.items as Item[]).reduce((acc, item) => {
                 if (item.type === BitOrbType.BIT_ORB_I || item.type === BitOrbType.BIT_ORB_II || item.type === BitOrbType.BIT_ORB_III) {
