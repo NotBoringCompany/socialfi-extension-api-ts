185f0ec360cd74ea087b7fcd72dbc189c2f43d7b
suwandre <suwandresukijat@gmail.com>
2024-07-12 21:55:59 +0200

    rf: removed some console logs

--- api/invite.ts
+++ api/invite.ts
@@ -307,8 +307,6 @@ export const updateSuccessfulIndirectReferrals = async (): Promise<void> => {
             return;
         }
 
-        console.log('User count: ', users.length);
-
         // get the successful indirect referrals
         const successfulIndirectReferrals = await SuccessfulIndirectReferralModel.find().lean();
 
@@ -346,8 +344,6 @@ export const updateSuccessfulIndirectReferrals = async (): Promise<void> => {
             const referredUsersReachedLevel4 = referredUsersData.filter(referredUserData => referredUserData.hasReachedLevel4);
 
             if (referredUsersReachedLevel4.length === 0) {
-                console.log(`User ${user._id} has no referred users that have reached level 4.`);
-
                 return;
             }
 
