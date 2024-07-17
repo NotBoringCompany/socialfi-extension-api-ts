5a154d1b26b61ab5e66744e023926c29faa11f05
suwandre <suwandresukijat@gmail.com>
2024-07-17 14:59:33 +0200

    rf: updated callback for twitter passport to use dev url

--- configs/passport.ts
+++ configs/passport.ts
@@ -7,7 +7,7 @@ passport.use(new TwitterStrategy({
     clientType: 'confidential',
     clientID: process.env.TWITTER_CLIENT_ID!,
     clientSecret: process.env.TWITTER_CLIENT_SECRET!,
-    callbackURL: process.env.TWITTER_CALLBACK_URL!,
+    callbackURL: process.env.TWITTER_CALLBACK_DEV_URL!,
     authorizationURL: 'https://x.com/i/oauth2/authorize',
     scope: ['tweet.read', 'users.read', 'offline.access'],
     },
