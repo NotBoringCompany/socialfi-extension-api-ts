c833c43cd047638ee83d5f3c2e5663ae52df1e0b
suwandre <suwandresukijat@gmail.com>
2024-07-20 21:53:21 +0200

    feat: check wonderbits account exists

--- utils/web3.ts
+++ utils/web3.ts
@@ -1,5 +1,5 @@
 import { ethers } from 'ethers';
-import { BLAST_TESTNET_PROVIDER, LOTTERY_CONTRACT } from './constants/web3'
+import { BLAST_TESTNET_PROVIDER, LOTTERY_CONTRACT, WONDERBITS_CONTRACT } from './constants/web3'
 
 /**
  * Gets the Lottery contract's balance; used for prize pool determination during draws.
@@ -10,6 +10,19 @@ export const getLotteryContractBalance = async (): Promise<number> => {
     // convert the balance in wei to ether
     return parseFloat(ethers.utils.formatEther(balance));
 }
+
+/**
+ * Checks if a given address has an account in the Wonderbits contract.
+ */
+export const checkWonderbitsAccountExists = async (address: string): Promise<boolean> => {
+    try {
+        const exists = await WONDERBITS_CONTRACT.playerExists(address);
+
+        return exists;
+    } catch (err: any) {
+        throw new Error(err.message);
+    }
+} 
 
 /**
  * Converts a random string into a Solidity bytes32 format.
