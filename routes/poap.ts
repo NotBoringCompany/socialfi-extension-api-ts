231908445a997c39189ba690cc2ff5f9c2af33d9
suwandre <suwandresukijat@gmail.com>
2024-07-22 14:11:28 +0200

    feat: POAP redeem has mixpanel increment

--- routes/poap.ts
+++ routes/poap.ts
@@ -3,6 +3,10 @@ import { Status } from '../utils/retVal';
 import { validateRequestAuth } from '../utils/auth';
 import { addPOAP, getAllPOAP, getUserPOAP, redeemCode } from '../api/poap';
 import { allowMixpanel, mixpanel } from '../utils/mixpanel';
+import { getMainWallet } from '../api/user';
+import { UserWallet } from '../models/user';
+import { WONDERBITS_CONTRACT } from '../utils/constants/web3';
+import { REDEEM_POAP_MIXPANEL_EVENT_HASH } from '../utils/constants/mixpanelEvents';
 
 const router = express.Router();
 
@@ -86,12 +90,35 @@ router.post('/redeem_poap', async (req, res) => {
         }
 
         const { status, message, data } = await redeemCode(validateData.twitterId, code);
+        let incrementCounterTxHash = '';
 
         if (status === Status.SUCCESS && allowMixpanel) {
             mixpanel.track('Redeem POAP', {
                 distinct_id: validateData?.twitterId,
                 '_code': code,
             });
+
+            // get the wallet address of the twitter ID
+            const { status: walletStatus, message: walletMessage, data: walletData } = await getMainWallet(validateData?.twitterId);
+
+            if (walletStatus !== Status.SUCCESS) {
+                // if there is an error somehow, ignore this and just return a success for the API endpoint
+                // as this is just an optional tracking feature.
+                return res.status(status).json({
+                    status,
+                    message,
+                    data: {
+                        ...data,
+                        incrementCounterTxHash
+                    }
+                })
+            }
+
+            const { address } = walletData.wallet as UserWallet;
+
+            // increment the counter for this mixpanel event on the wonderbits contract
+            const incrementCounterTx = await WONDERBITS_CONTRACT.incrementEventCounter(address, REDEEM_POAP_MIXPANEL_EVENT_HASH);
+            incrementCounterTxHash = incrementCounterTx.hash;
         }
 
         return res.status(status).json({
