d8f3085c94b25c4b780d1a716f98e6f08cfaf0ba
suwandre <suwandresukijat@gmail.com>
2024-07-22 13:55:34 +0200

    feat: consume bit orb now has mixpanel counter increment

--- routes/bitOrb.ts
+++ routes/bitOrb.ts
@@ -3,6 +3,10 @@ import { consumeBitOrb } from '../api/bitOrb';
 import { validateRequestAuth } from '../utils/auth';
 import { Status } from '../utils/retVal';
 import { allowMixpanel, mixpanel } from '../utils/mixpanel';
+import { UserWallet } from '../models/user';
+import { WONDERBITS_CONTRACT } from '../utils/constants/web3';
+import { CONSUME_BIT_ORB_MIXPANEL_EVENT_HASH } from '../utils/constants/mixpanelEvents';
+import { getMainWallet } from '../api/user';
 
 const router = express.Router();
 
@@ -25,6 +29,7 @@ router.post('/consume', async (req, res) => {
             validateData?.twitterId,
             type
         );
+        let incrementCounterTxHash = '';
 
         if (status === Status.SUCCESS && allowMixpanel) {
             mixpanel.track('Consume Bit Orb', {
@@ -32,6 +37,28 @@ router.post('/consume', async (req, res) => {
                 '_type': type,
                 '_bit': data?.bit,
             });
+
+            // get the wallet address of the twitter ID
+            const { status: walletStatus, message: walletMessage, data: walletData } = await getMainWallet(validateData?.twitterId);
+
+            if (walletStatus !== Status.SUCCESS) {
+                // if there is an error somehow, ignore this and just return a success for the API endpoint
+                // as this is just an optional tracking feature.
+                return res.status(status).json({
+                    status,
+                    message,
+                    data: {
+                        ...data,
+                        incrementCounterTxHash
+                    }
+                })
+            }
+
+            const { address } = walletData.wallet as UserWallet;
+
+            // increment the counter for this mixpanel event on the wonderbits contract
+            const incrementCounterTx = await WONDERBITS_CONTRACT.incrementEventCounter(address, CONSUME_BIT_ORB_MIXPANEL_EVENT_HASH);
+            incrementCounterTxHash = incrementCounterTx.hash;
         }
 
         return res.status(status).json({
