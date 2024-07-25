5757c72d049c00be427c005e2eff6158fce05751
suwandre <suwandresukijat@gmail.com>
2024-07-25 22:47:50 +0200

    feat: sendKICKUponRegistration api

--- api/web3.ts
+++ api/web3.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { WONDERBITS_CONTRACT } from '../utils/constants/web3';
 import { ReturnValue, Status } from '../utils/retVal';
 
@@ -29,6 +30,43 @@ export const checkWonderbitsAccountRegistrationRequired = async (address: string
         return {
             status: Status.ERROR,
             message: `(createWonderbitsAccount) ${err.message}`
+        }
+    }
+}
+
+/**
+ * Sends a user some KICK tokens for XProtocol Testnet upon registering their account via Twitter.
+ */
+export const sendKICKUponRegistration = async (walletAddress: string): Promise<ReturnValue> => {
+    try {
+        const response = await axios.post(
+            `https://staging.xprotocol.org/api/faucets-request`,
+            {
+                addresses: [walletAddress]
+            },
+            {
+                headers: {
+                    'x-api-key': process.env.X_PROTOCOL_TESTNET_FAUCET_KEY!,
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+
+        if (response.data.ok) {
+            return {
+                status: Status.SUCCESS,
+                message: `(sendKICKUponRegistration) 1 KICK sent successfully.`,
+            }
+        } else {
+            return {
+                status: Status.ERROR,
+                message: `(sendKICKUponRegistration) Failed to send KICK tokens.`
+            }
+        }
+    } catch (err: any) {
+        return {
+            status: Status.ERROR,
+            message: `(sendKICKUponRegistration) ${err.message}`
         }
     }
 }
