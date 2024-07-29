59273d6ce4c18628577a226dbc8721f9f25e2a4e
Dwa Meizadewa <infamous0192@gmail.com>
2024-07-29 22:23:07 +0800

    rf: fixed handleTelegramLogin

--- api/user.ts
+++ api/user.ts
@@ -2540,6 +2540,22 @@ export const handleTelegramLogin = async (initData: string): Promise<ReturnValue
             // creates the wallet for the user
             const { privateKey, address } = createUserWallet();
 
+            // initialize PlayerEnergy for new user
+            const newEnergy: PlayerEnergy = {
+                currentEnergy: MAX_ENERGY_CAP,
+                maxEnergy: MAX_ENERGY_CAP,
+                dailyEnergyPotion: MAX_ENERGY_POTION_CAP,
+            };
+
+            // initialize PlayerMastery for new user
+            const newMastery: PlayerMastery = {
+                tapping: {
+                    level: 1,
+                    totalExp: 0,
+                    rerollCount: 6,
+                },
+            };
+
             const newUser = new UserModel({
                 _id: userObjectId,
                 twitterId: telegramData.user.id,
@@ -2595,6 +2611,8 @@ export const handleTelegramLogin = async (initData: string): Promise<ReturnValue
                 },
                 inGameData: {
                     level: 1,
+                    energy: newEnergy,
+                    mastery: newMastery,
                     completedTutorialIds: [],
                     beginnerRewardData: {
                         lastClaimedTimestamp: 0,
