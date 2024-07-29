052dc4d15a634e59148f3697f8e5527d2f6d3d79
suwandre <suwandresukijat@gmail.com>
2024-07-29 23:47:38 +0200

    rf: uncommented schedulers

--- server.ts
+++ server.ts
@@ -87,5 +87,5 @@ app.use('/collab', checkMaintenance, collabV2);
 app.listen(port, async () => {
     console.log(`Server running on port ${port}`);
 
-    // await schedulers();
+    await schedulers();
 });
