924a557e4ca71cd3ecdd01f6620184cac262e8a1
suwandre <suwandresukijat@gmail.com>
2024-07-31 23:08:39 +0200

    feat: encryption and decryption for private keys of web3 wallets

--- utils/crypto.ts
+++ utils/crypto.ts
@@ -1,4 +1,5 @@
 import CryptoJS from 'crypto-js';
+import crypto from 'crypto';
 import { BLAST_TESTNET_PROVIDER } from './constants/web3';
 import { ReturnValue, Status } from './retVal';
 import { ethers } from 'ethers';
@@ -104,7 +105,7 @@ export const generateStarterCode = (): string => {
  */
 export const encrypt = (data: string): string => {
     const secretKey = CryptoJS.enc.Utf8.parse(process.env.ENCRYPTION_KEY!);
-    const iv = CryptoJS.enc.Utf8.parse(process.env.IV); // IV should be 16 bytes
+    const iv = CryptoJS.enc.Utf8.parse(process.env.IV_LENGTH!);
     const encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(data), secretKey, {
         iv: iv,
         mode: CryptoJS.mode.CBC,
@@ -118,11 +119,33 @@ export const encrypt = (data: string): string => {
  */
 export const decrypt = (cipherText: string) => {
     const secretKey = CryptoJS.enc.Utf8.parse(process.env.ENCRYPTION_KEY);
-    const iv = CryptoJS.enc.Utf8.parse(process.env.IV); // IV should be 16 bytes
+    const iv = CryptoJS.enc.Utf8.parse(process.env.IV_LENGTH!);
     const decrypted = CryptoJS.AES.decrypt(cipherText, secretKey, {
         iv: iv,
         mode: CryptoJS.mode.CBC,
         padding: CryptoJS.pad.Pkcs7
     });
     return CryptoJS.enc.Utf8.stringify(decrypted);
+}
+
+/** Encrypts a private key */
+export const encryptPrivateKey = (privateKey: string): string => {
+    const iv = crypto.randomBytes(parseInt(process.env.IV_LENGTH!));
+    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(process.env.ENCRYPTION_KEY!, 'hex'), iv);
+    let encrypted = cipher.update(privateKey, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+
+    return iv.toString('hex') + ':' + encrypted;
+}
+
+/** Decrypts a private key */
+export const decryptPrivateKey = (encryptedPrivateKey: string): string => {
+    const parts = encryptedPrivateKey.split(':');
+    const iv = Buffer.from(parts.shift() as string, 'hex');
+    const encryptedText = Buffer.from(parts.join(':'), 'hex');
+    const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(process.env.ENCRYPTION_KEY!, 'hex'), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+    return decrypted.toString('utf8'); 
 }
