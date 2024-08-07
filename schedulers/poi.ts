0bfea4c2b95c29cca026351f466ebf236c58441c
suwandre <suwandresukijat@gmail.com>
2024-08-07 11:23:32 +0200

    rf: updated reset global items buyable and sellable amount to get called at 11:59 UTC as well

--- schedulers/poi.ts
+++ schedulers/poi.ts
@@ -3,13 +3,17 @@ import { checkDailyKOSRewards, checkWeeklyKOSRewards } from '../api/kos';
 import { resetGlobalItemsDailyBuyableAndSellableAmount } from '../api/poi';
 
 /**
- * Calls `resetGlobalItemsDailyBuyableAndSellableAmount` every day at 23:59 UTC to reset the daily buyable and sellable amount of global items.
+ * Calls `resetGlobalItemsDailyBuyableAndSellableAmount` every day at 11:59 UTC and 23:59 UTC to reset the daily buyable and sellable amount of global items.
  */
 export const resetGlobalItemsDailyBuyableAndSellableAmountScheduler = async (): Promise<void> => {
     try {
-        cron.schedule('59 23 * * *', async () => {
-            console.log('Running resetGlobalItemsDailyBuyableAndSellableAmount...');
-            await resetGlobalItemsDailyBuyableAndSellableAmount();
+        const schedules = ['59 23 * * *', '59 11 * * *'];
+        
+        schedules.forEach(schedule => {
+            cron.schedule(schedule, async () => {
+                console.log('Running resetGlobalItemsDailyBuyableAndSellableAmount...');
+                await resetGlobalItemsDailyBuyableAndSellableAmount();
+            });
         });
     } catch (err: any) {
         console.error('Error in resetGlobalItemsDailyBuyableAndSellableAmount:', err.message);
