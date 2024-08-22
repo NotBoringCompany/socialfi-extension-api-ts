19a0c846121feeda5128e31c26b63487bdae1d73
Dwa Meizadewa <infamous0192@gmail.com>
2024-08-22 22:53:03 +0800

    rf: fixed user backup data

--- api/user.ts
+++ api/user.ts
@@ -2937,7 +2937,10 @@ export const handleTelegramConnect = async (twitterId: string, telegramUser: Tel
             };
         } else {
             // backup the data
-            // await UserBackupModel.create(registeredTelegram);
+            await UserBackupModel.create({
+                ...registeredTelegram,
+                _id: generateObjectId()
+            });
 
             // if the user's confirmed then delete the existed telegram account
             await registeredTelegram.deleteOne();
