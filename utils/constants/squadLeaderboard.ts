6f4897833970b87e824b2d0180b76495822079d3
Christian-Liyanto <christian.liyanto@student.umn.ac.id>
2024-09-02 15:04:51 +0700

    rf: Squad Weekly Ranking adjustment

--- utils/constants/squadLeaderboard.ts
+++ utils/constants/squadLeaderboard.ts
@@ -5,17 +5,17 @@ import { SquadReward, SquadRewardType } from '../../models/squadLeaderboard';
  * Gets a squad's new weekly ranking based on the points earned the previous week.
  */
 export const GET_SQUAD_WEEKLY_RANKING = (points: number): SquadRank => {
-    if (points >= 1 && points <= 99999) {
+    if (points >= 0 && points <= 1999) {
         return SquadRank.UNRANKED;
-    } else if (points >= 100000 && points <= 999999) {
+    } else if (points >= 2000 && points <= 4999) {
         return SquadRank.BRONZE;
-    } else if (points >= 1000000 && points <= 4999999) {
+    } else if (points >= 5000 && points <= 19999) {
         return SquadRank.SILVER;
-    } else if (points >= 5000000 && points <= 19999999) {
+    } else if (points >= 20000 && points <= 79999) {
         return SquadRank.GOLD;
-    } else if (points >= 20000000 && points <= 49999999) {
+    } else if (points >= 80000 && points <= 159999) {
         return SquadRank.PLATINUM;
-    } else if (points >= 50000000 && points <= 99999999) {
+    } else if (points >= 160000 && points <= 299999) {
         return SquadRank.DIAMOND;
     } else {
         return SquadRank.MASTER;
