2f90df13c8e6d44bfe2f77632a02bf94f62e8702
Manan <manan.jabrik@gmail.com>
2024-09-16 15:30:27 +0700

    feat: add routes, change ban response

--- api/ban.ts
+++ api/ban.ts
@@ -1,8 +1,37 @@
 import { BanStatus, BanType } from "../models/ban"
+import { ReturnValue } from "../utils/retVal"
 
 // ban service 
-export const getBans = async () => {}
-export const getBanById = async (banId: string) => {}
-export const getBanByUserId = async (userId:string) => {}
-export const addBan = async (userId:string, banType:BanType, startDate?:Date, endDate?:Date, reason?:string, adminId?:string, status?:BanStatus) => {}
-export const updateBan = async (banId:string, banType:BanType, startDate?:Date, endDate?:Date, reason?:string, adminId?:string, status?:BanStatus) => {}
+export const getBans = async (): Promise<ReturnValue> => {
+
+  return {
+    status: 200,
+    message: "success",
+  }
+}
+export const getBanById = async (banId: string): Promise<ReturnValue> => {
+  return {
+    status: 200,
+    message: "success",
+
+  }
+}
+export const getBanByUserId = async (userId: string): Promise<ReturnValue> => {
+  return {
+    status: 200,
+    message: "success",
+  }
+}
+export const addBan = async (userId: string, banType: BanType, startDate?: Date, endDate?: Date, reason?: string, adminId?: string, status?: BanStatus): Promise<ReturnValue> => {
+  return {
+    status: 200,
+    message: "success",
+
+  }
+}
+export const updateBan = async (banId: string, banType: BanType, startDate?: Date, endDate?: Date, reason?: string, adminId?: string, status?: BanStatus): Promise<ReturnValue> => {
+  return {
+    status: 200,
+    message: "success",
+  }
+}
