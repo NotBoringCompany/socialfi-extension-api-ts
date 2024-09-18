230e4192c91b73fbed1b378446eaff1eedf8f8aa
Christian-Liyanto <christian.liyanto@student.umn.ac.id>
2024-09-18 09:33:13 +0700

    feat: /get_craft_assets endpoint

--- routes/craft.ts
+++ routes/craft.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { Status } from '../utils/retVal';
 import { validateRequestAuth } from '../utils/auth';
 import { craftAsset } from '../api/craft';
+import { CRAFTING_RECIPES } from '../utils/constants/craft';
 
 const router = express.Router();
 
@@ -24,6 +25,32 @@ router.post('/craft_asset', async (req, res) => {
             status,
             message,
             data
+        });
+    } catch (err: any) {
+        return res.status(500).json({
+            status: 500,
+            message: err.message
+        })
+    }
+});
+
+router.get('/get_craft_assets', async (req, res) => {
+    try {
+        const { status: validateStatus, message: validateMessage, data: validateData } = await validateRequestAuth(req, res, 'get_craft_assets');
+
+        if (validateStatus !== Status.SUCCESS) {
+            return res.status(validateStatus).json({
+                status: validateStatus,
+                message: validateMessage
+            })
+        }
+
+        return res.status(200).json({
+            status: 200,
+            message: 'Succesfully retrieved craftable assets',
+            data: {
+                craftableAsset: CRAFTING_RECIPES,
+            }
         });
     } catch (err: any) {
         return res.status(500).json({
@@ -62,37 +89,5 @@ router.post('/craft_asset', async (req, res) => {
 //         })
 //     }
 // });
-
-// router.get('/get_all_craft', async (req, res) => {
-    
-
-//     try {
-//         const { status: validateStatus, message: validateMessage, data: validateData } = await validateRequestAuth(req, res, 'get_all_craft');
-
-//         if (validateStatus !== Status.SUCCESS) {
-//             return res.status(validateStatus).json({
-//                 status: validateStatus,
-//                 message: validateMessage
-//             })
-//         }
-
-//         //const { status, message, data } = await feedBit(validateData?.twitterId, bitId, <FoodType>foodType);
-//         const { status, message, data } = await getAllCraftingRecipes();
-        
-
-//         return res.status(status).json({
-//             status,
-//             message,
-//             data
-//         });
-//     } catch (err: any) {
-//         return res.status(500).json({
-//             status: 500,
-//             message: err.message
-//         })
-//     }
-// });
-
-
 
 export default router;
