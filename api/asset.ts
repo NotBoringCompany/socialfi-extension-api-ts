10109e9023e9e35112df66d34c73fad87d465603
Suwandre <suwandresukijat@gmail.com>
2024-09-27 13:30:58 +0200

    rf: updated recipe for transmutation items

---
+++ api/asset.ts
@@ -0,0 +1,6 @@
+import { RestorationItem } from '../models/item';
+import { ReturnValue } from '../utils/retVal';
+
+// export const consumeRestorationItem = async (twitterId: string, item: RestorationItem): Promise<ReturnValue> => {
+
+// }
