a3b5faa3af31fcc8bdf0b0a239c7706fef05aa0a
Manan <manan.jabrik@gmail.com>
2024-10-04 16:01:06 +0700

    rf: change global key name

--- socket/index.ts
+++ socket/index.ts
@@ -1,12 +1,12 @@
 import { Socket } from "socket.io";
-import { RedisKey, startRest, stopRest } from "../api/sauna";
-import { EventSauna } from "../models/sauna";
+import {  startRest, stopRest } from "../api/sauna";
+import { EventSauna, SaunaGlobalKey } from "../models/sauna";
 import redisDb from "../utils/constants/redisDb";
 
 export const socketHandler = (socket: Socket) => {
   console.log('a user connected');
   socket.on("connect", () => {
-    redisDb.get(RedisKey.CONNECTED).then((userConnected) => {
+    redisDb.get(SaunaGlobalKey.CONNECTED).then((userConnected) => {
       socket.broadcast.emit(EventSauna.USER_COUNT, userConnected ? Number(userConnected) : 0)
       socket.emit(EventSauna.USER_COUNT, userConnected ? Number(userConnected) : 0)
     }).catch((error) => {
