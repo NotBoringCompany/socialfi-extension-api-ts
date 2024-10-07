0f99e09699fc8574336fdf115378469900cff49e
Dwa Meizadewa <infamous0192@gmail.com>
2024-10-08 03:33:46 +0800

    rf: added counter initializer on redis connect

--- utils/constants/redis.ts
+++ utils/constants/redis.ts
@@ -1,3 +1,19 @@
 import Redis from 'ioredis';
+import { getLatestBitId } from '../../api/bit';
+import { getLatestIslandId } from '../../api/island';
+import { getLatestRaftId } from '../../api/raft';
 
 export const redis = new Redis(process.env.REDIS_URL);
+
+redis.on('connect', async () => {
+    console.log('Redis connected');
+
+    // initialize bit latest id counter
+    getLatestBitId();
+
+    // initialize island latest id counter
+    getLatestIslandId();
+
+    // initialize raft latest id counter
+    getLatestRaftId();
+});
