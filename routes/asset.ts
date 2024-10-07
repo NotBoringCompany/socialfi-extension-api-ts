8835d3c5a4670f827186d67e22e4f33daa5bd062
Suwandre <suwandresukijat@gmail.com>
2024-10-07 19:26:31 +0200

    feat: consume synthesizing item route

--- routes/asset.ts
+++ routes/asset.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import { assets } from '../utils/constants/asset';
+import { validateRequestAuth } from '../utils/auth';
+import { Status } from '../utils/retVal';
+import { consumeSynthesizingItem } from '../api/asset';
 
 const router = express.Router();
 
@@ -18,5 +21,34 @@ router.get('/get_asset_descriptions', async (req, res) => {
             message: err.message
         })
     }
-})
+});
+
+router.post('/consume_synthesizing_item', async (req, res) => {
+    const { item, islandOrBitId, newResourceLine, chosenBitTraitsToReroll } = req.body;
+
+    try {
+        const { status: validateStatus, message: validateMessage, data: validateData } = await validateRequestAuth(req, res, 'consume_synthesizing_item');
+
+        if (validateStatus !== Status.SUCCESS) {
+            return res.status(validateStatus).json({
+                status: validateStatus,
+                message: validateMessage
+            })
+        }
+
+        const { status, message, data } = await consumeSynthesizingItem(validateData?.twitterId, item, islandOrBitId, newResourceLine, chosenBitTraitsToReroll);
+
+        return res.status(status).json({
+            status,
+            message,
+            data
+        });
+    } catch (err: any) {
+        return res.status(500).json({
+            status: 500,
+            message: err.message
+        })
+    }
+});
+
 export default router;
