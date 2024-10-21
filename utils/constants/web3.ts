5bb7de67b5db5e43ffd78979bb47c1497db102b6
Suwandre <suwandresukijat@gmail.com>
2024-10-21 18:20:00 +0200

    rf: removed old lottery related logic

--- utils/constants/web3.ts
+++ utils/constants/web3.ts
@@ -32,7 +32,6 @@ export const USER_WALLET = (provider: ethers.providers.JsonRpcProvider, privateK
     return new ethers.Wallet(privateKey, provider);
 }
 
-export const LOTTERY_CONTRACT_ADDRESS = process.env.LOTTERY_CONTRACT!;
 export const COOKIE_CONTRACT_ADDRESS = process.env.COOKIE_CONTRACT!;
 export const COOKIE_CONTRACT_DECIMALS = 8;
 
@@ -42,12 +41,6 @@ export const SUPERIOR_KEYCHAIN_CONTRACT_ADDRESS = process.env.SUPERIOR_KEYCHAIN_
 
 // wonderbits address in XProtocol testnet
 export const WONDERBITS_CONTRACT_ADDRESS = process.env.WONDERBITS_CONTRACT!;
-
-export const LOTTERY_ARTIFACT = JSON.parse(
-    fs.readFileSync(
-        path.join(__dirname, '../../artifacts/Lottery.json')
-    ).toString()
-);
 
 export const COOKIE_ARTIFACT = JSON.parse(
     fs.readFileSync(
@@ -77,15 +70,6 @@ export const WONDERBITS_ARTIFACT = JSON.parse(
     fs.readFileSync(
         path.join(__dirname, '../../artifacts/Wonderbits.json')
     ).toString()
-);
-
-/**
- * The lottery contract instance (using admin wallet)
- */
-export const LOTTERY_CONTRACT = new ethers.Contract(
-    LOTTERY_CONTRACT_ADDRESS,
-    LOTTERY_ARTIFACT.abi,
-    DEPLOYER_WALLET(BLAST_TESTNET_PROVIDER)
 );
 
 /**
@@ -132,19 +116,6 @@ export const WONDERBITS_CONTRACT = new ethers.Contract(
     WONDERBITS_ARTIFACT.abi,
     DEPLOYER_WALLET(XPROTOCOL_TESTNET_PROVIDER)
 );
-
-/**
- * The lottery contract instance (using user wallet, requires their private key)
- */
-export const LOTTERY_CONTRACT_USER = (privateKey: string) => {
-    const wallet = new ethers.Wallet(privateKey, BLAST_TESTNET_PROVIDER);
-
-    return new ethers.Contract(
-        LOTTERY_CONTRACT_ADDRESS,
-        LOTTERY_ARTIFACT.abi,
-        wallet
-    );
-}
 
 /**
  * The cookie contract instance (using user wallet, requires their private key)
--- utils/web3.ts
+++ utils/web3.ts
@@ -1,15 +1,4 @@
 import { ethers } from 'ethers';
-import { BLAST_TESTNET_PROVIDER, LOTTERY_CONTRACT } from './constants/web3'
-
-/**
- * Gets the Lottery contract's balance; used for prize pool determination during draws.
- */
-export const getLotteryContractBalance = async (): Promise<number> => {
-    const balance = await BLAST_TESTNET_PROVIDER.getBalance(LOTTERY_CONTRACT.address);
-
-    // convert the balance in wei to ether
-    return parseFloat(ethers.utils.formatEther(balance));
-}
 
 /**
  * Converts a random string into a Solidity bytes32 format.
