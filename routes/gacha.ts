2774a03221288ec3a256b917d28c3f851e69fa20
Suwandre <suwandresukijat@gmail.com>
2024-10-24 22:28:02 +0200

    feat: roll wonderspin route

--- routes/gacha.ts
+++ routes/gacha.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { validateRequestAuth } from '../utils/auth';
 import { Status } from '../utils/retVal';
 import { authMiddleware } from '../middlewares/auth';
-import { addWonderspin } from '../api/gacha';
+import { addWonderspin, rollWonderspin } from '../api/gacha';
 
 const router = express.Router();
 
@@ -23,5 +23,32 @@ router.post('/add_wonderspin', authMiddleware(3), async (req, res) => {
         })
     }
 });
+
+router.post('/roll_wonderspin', async (req, res) => {
+    const { ticket, wonderspin, amount } = req.body;
+    try {
+        const { status: validateStatus, message: validateMessage, data: validateData } = await validateRequestAuth(req, res, 'roll_wonderspin');
+        
+        if (validateStatus !== Status.SUCCESS) {
+            return res.status(validateStatus).json({
+                status: validateStatus,
+                message: validateMessage
+            });
+        }
+        
+        const { status, message, data } = await rollWonderspin(validateData?.twitterId, ticket, wonderspin, amount);
+
+        return res.status(status).json({
+            status,
+            message,
+            data
+        });
+    } catch (err: any) {
+        return res.status(500).json({
+            status: 500,
+            message: err.message
+        });
+    }
+})
 
 export default router;
