abce614809885044107ce0166cffb2c3af3b750d
Suwandre <suwandresukijat@gmail.com>
2024-10-28 15:11:31 +0100

    rf: added delete redis lock key

--- api/gacha.ts
+++ api/gacha.ts
@@ -700,5 +700,7 @@ export const rollWonderspin = async (
             status: Status.ERROR,
             message: `(rollWonderspin) ${err.message}`
         }
+    } finally {
+        await redis.del(lockKey);
     }
 }
