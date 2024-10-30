cf35d4395b0a7bc4cb970c61e6e47930321717c4
Christian-Liyanto <christian.liyanto@student.umn.ac.id>
2024-10-30 20:11:58 +0700

    feat: Added new constant for DailyQuest/BerryFactory

--- utils/constants/quest.ts
+++ utils/constants/quest.ts
@@ -1,4 +1,6 @@
 import { FoodType } from '../../models/food';
+import { BerryFactoryMasteryStats } from '../../models/mastery';
+import { POIName } from '../../models/poi';
 import { dayjs } from '../dayjs';
 
 /** gets the corresponding food from completing a quest based on the probability of obtaining it, depending on `rand`, which is a number from 1 to 100 */
@@ -55,4 +57,125 @@ export const DAILY_QUEST_LAPSE_PHASE = () => {
         DAILY_QUEST_INIT_LAPSE + DAILY_QUEST_LAPSE_TIME * Math.floor(divide),
         DAILY_QUEST_INIT_LAPSE + DAILY_QUEST_LAPSE_TIME * Math.ceil(divide),
     ];
+};
+
+/**
+ * Return Extra quest benefit that user can accept per 48 Hours
+ */
+export const EXTRA_QUESTS_BENEFIT = (factoryMastery: {
+    evergreenVillage: BerryFactoryMasteryStats | null;
+    palmshadeVillage: BerryFactoryMasteryStats | null;
+    seabreezeHarbor: BerryFactoryMasteryStats | null;
+}) => {
+    // Initialize totalExtra count
+    let totalExtra: number = 0;
+    // Destructure factoryMastery
+    const { evergreenVillage, palmshadeVillage, seabreezeHarbor } = factoryMastery;
+
+    // handle case for evergreenVillage
+    if (evergreenVillage) {
+        const { level } = evergreenVillage;
+        if (level < 10) totalExtra += 0;
+        else if (level < 20) totalExtra += 1;
+        else totalExtra += 2;
+    }
+
+    // handle case for palmshadeVillage
+    if (palmshadeVillage) {
+        const { level } = palmshadeVillage;
+        if (level < 10) totalExtra += 0;
+        else if (level < 20) totalExtra += 1;
+        else totalExtra += 2;
+    }
+
+    // handle case for seabreezeHarbor
+    if (seabreezeHarbor) {
+        const { level } = seabreezeHarbor;
+        if (level < 10) totalExtra += 0;
+        else if (level < 20) totalExtra += 1;
+        else totalExtra += 2;
+    }
+
+    return totalExtra;
+};
+
+/**
+ * Return Extra earning benefits based on Level & POIName passed.
+ * Extra Benefit will only affect respective POI only
+ */
+export const EXTRA_LOCAL_EARNING_BENEFIT = (level: number, poiName: POIName) => {
+    switch(poiName) {
+        case POIName.EVERGREEN_VILLAGE: {
+            if (level < 5) return 0;
+            else return 1;
+        }
+        case POIName.PALMSHADE_VILLAGE:
+            if (level < 5) return 0;
+            else return 2;
+        case POIName.SEABREEZE_HARBOR:
+            if (level < 5) return 0;
+            else return 3;
+        // For POI that isn't specified or invalid will return 0 as Benefit
+        default: return 0;
+    }
+};
+
+/**
+ * Return Daily Quest Level Up Rewards
+ */
+export const DAILY_QUEST_LEVEL_UP_REWARDS = ( level: number, poiName: POIName ): { xCookies: number, cheques: number } => {
+    switch(poiName) {
+        case POIName.EVERGREEN_VILLAGE: {
+            if (level >= 2 && level <= 4) {
+                return { xCookies: 5, cheques: 10 };
+            } else if (level === 5) {
+                return { xCookies: 20, cheques: 20 };
+            } else if (level >= 6 && level <= 9) {
+                return { xCookies: 10, cheques: 10 };
+            } else if (level === 10) {
+                return { xCookies: 30, cheques: 30 };
+            } else if (level >= 11 && level <= 19) {
+                return { xCookies: 15, cheques: 10 };
+            } else if (level === 20) {
+                return { xCookies: 40, cheques: 30 };
+            } else {
+                return { xCookies: 0, cheques: 0 };
+            }
+        }
+        case POIName.PALMSHADE_VILLAGE: {
+            if (level >= 2 && level <= 4) {
+                return { xCookies: 10, cheques: 50 };
+            } else if (level === 5) {
+                return { xCookies: 30, cheques: 100 };
+            } else if (level >= 6 && level <= 9) {
+                return { xCookies: 15, cheques: 50 };
+            } else if (level === 10) {
+                return { xCookies: 40, cheques: 150 };
+            } else if (level >= 11 && level <= 19) {
+                return { xCookies: 20, cheques: 50 };
+            } else if (level === 20) {
+                return { xCookies: 50, cheques: 150 };
+            } else {
+                return { xCookies: 0, cheques: 0 };
+            }
+        }
+        case POIName.SEABREEZE_HARBOR: {
+            if (level >= 2 && level <= 4) {
+                return { xCookies: 20, cheques: 200 };
+            } else if (level === 5) {
+                return { xCookies: 40, cheques: 400 };
+            } else if (level >= 6 && level <= 9) {
+                return { xCookies: 20, cheques: 200 };
+            } else if (level === 10) {
+                return { xCookies: 50, cheques: 600 };
+            } else if (level >= 11 && level <= 19) {
+                return { xCookies: 25, cheques: 200 };
+            } else if (level === 20) {
+                return { xCookies: 60, cheques: 600 };
+            } else {
+                return { xCookies: 0, cheques: 0 };
+            }
+        }
+        default: return { xCookies: 0, cheques: 0 };
+    }
 };
