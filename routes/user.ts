c7de282806160da1e7c25c0cb1519917be306811
Dwa Meizadewa <infamous0192@gmail.com>
2024-11-08 16:25:37 +0800

    rf: added get_user_profile route

--- routes/user.ts
+++ routes/user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { checkInviteCodeLinked, claimBeginnerRewards, claimDailyRewards, consumeEnergyPotion, generateSignatureMessage, generateUnlinkSignatureMessage, getBeginnerRewardsData, getInGameData, getInventory, getMainWallet, getUserData, getWalletDetails, linkInviteCode, linkSecondaryWallet, removeResources, unlinkSecondaryWallet } from '../api/user';
+import { checkInviteCodeLinked, claimBeginnerRewards, claimDailyRewards, consumeEnergyPotion, generateSignatureMessage, generateUnlinkSignatureMessage, getBeginnerRewardsData, getInGameData, getInventory, getMainWallet, getUserData, getUserProfile, getWalletDetails, linkInviteCode, linkSecondaryWallet, removeResources, unlinkSecondaryWallet } from '../api/user';
 import { validateRequestAuth } from '../utils/auth';
 import { Status } from '../utils/retVal';
 import { ExtendedProfile } from '../utils/types';
@@ -425,5 +425,33 @@ router.post('/consume_energy_potion', async (req, res) => {
         return res.status(500).json({ status: 500, message: err.message });
     }
 })
+
+router.post('/get_user_profile/:twitterId', async (req, res) => {
+    const { twitterId } = req.params;
+
+    try {
+        const { status: validateStatus, message: validateMessage, data: validateData } = await validateRequestAuth(req, res, 'get_user_profile');
+
+        if (validateStatus !== Status.SUCCESS) {
+            return res.status(validateStatus).json({
+                status: validateStatus,
+                message: validateMessage
+            })
+        }
+
+        const { status, message, data } = await getUserProfile(twitterId);
+
+        return res.status(status).json({
+            status,
+            message,
+            data
+        });
+    } catch (err: any) {
+        return res.status(500).json({
+            status: 500,
+            message: err.message
+        });
+    }
+});
 
 export default router;
