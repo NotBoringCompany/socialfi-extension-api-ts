43e1e9548811b3f4443ea2a5a3ebfe65dff204bc
Suwandre <suwandresukijat@gmail.com>
2024-11-09 11:27:56 +0100

    rf: updated validateRequestAuth logic

--- utils/auth.ts
+++ utils/auth.ts
@@ -43,6 +43,13 @@ export const validateRequestAuth = async (
             }
         }
 
+        if (req.body.twitterId && twitterId !== req.body.twitterId) {
+            return {
+                status: Status.UNAUTHORIZED,
+                message: `(${routeName ?? 'validateRequestAuth'}) Twitter ID does not match the request body.`
+            }
+        }
+
         // at this point, the token is deemed valid and we return the twitter id.
         return {
             status: Status.SUCCESS,
