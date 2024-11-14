31cf9ddc4cafaf3639204df4486039f244e4a9ce
Suwandre <suwandresukijat@gmail.com>
2024-11-14 19:51:53 +0100

    rf: deleted asset data with descriptions

--- routes/asset.ts
+++ routes/asset.ts
@@ -1,27 +1,9 @@
 import express from 'express';
-import { assets } from '../utils/constants/asset';
 import { validateRequestAuth } from '../utils/auth';
 import { Status } from '../utils/retVal';
 import { consumeSynthesizingItem } from '../api/asset';
 
 const router = express.Router();
-
-router.get('/get_asset_descriptions', async (req, res) => {
-    try {
-        return res.status(200).json({
-            status: 200,
-            message: 'Asset descriptions retrieved successfully.',
-            data: {
-                assets
-            }
-        })
-    } catch (err: any) {
-        return res.status(500).json({
-            status: 500,
-            message: err.message
-        })
-    }
-});
 
 router.post('/consume_synthesizing_item', async (req, res) => {
     const { item, islandOrBitId, newIslandTraits, chosenBitTraitsToReroll } = req.body;
