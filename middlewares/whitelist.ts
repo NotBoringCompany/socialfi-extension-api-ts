67eacef19e3f7f51343f6bed7960131fe8f1981b
Dwa Meizadewa <infamous0192@gmail.com>
2024-11-20 19:54:01 +0800

    rf: fixed import order

--- middlewares/whitelist.ts
+++ middlewares/whitelist.ts
@@ -6,51 +6,49 @@ import { SettingModel } from '../utils/constants/db';
 /**
  * Check if the user got whitelisted.
  */
-export const whitelistMiddleware = () => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        res.header('Access-Control-Allow-Methods', 'POST');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+export const whitelistMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-        try {
-            const keySetting = await SettingModel.findOne({ key: 'whitelist-status' });
+    try {
+        const keySetting = await SettingModel.findOne({ key: 'whitelist-status' });
 
-            if (!keySetting.value) return next();
+        if (!keySetting.value) return next();
 
-            const userSetting = await SettingModel.findOne({ key: 'whitelist-user' });
+        const userSetting = await SettingModel.findOne({ key: 'whitelist-user' });
 
-            const twitterIds = (userSetting.value as string).split(',');
+        const twitterIds = (userSetting.value as string).split(',');
 
-            const authHeader = req.headers.authorization;
-            const token = authHeader && authHeader.split(' ')[1]; // bearer JWT token
+        const authHeader = req.headers.authorization;
+        const token = authHeader && authHeader.split(' ')[1]; // bearer JWT token
 
-            if (!token) {
-                return next();
-            }
-
-            const { status, message, data } = validateJWT(token);
-            if (status !== Status.SUCCESS) {
-                return {
-                    status,
-                    message: `(whitelistMiddleware) ${message}`,
-                };
-            }
-
-            const { twitterId } = data;
-
-            if (!twitterId || !twitterIds.includes(twitterId)) {
-                return {
-                    status: Status.UNAUTHORIZED,
-                    message: `(whitelistMiddleware) You denied the app or the token is invalid/expired.`,
-                };
-            }
-
-            next();
-        } catch (err: any) {
-            console.error(`(whitelistMiddleware) ${err.message}`);
-            return res.status(500).json({
-                status: 500,
-                message: err.message,
-            });
+        if (!token) {
+            return next();
         }
-    };
+
+        const { status, message, data } = validateJWT(token);
+        if (status !== Status.SUCCESS) {
+            return {
+                status,
+                message: `(whitelistMiddleware) ${message}`,
+            };
+        }
+
+        const { twitterId } = data;
+
+        if (!twitterId || !twitterIds.includes(twitterId)) {
+            return {
+                status: Status.UNAUTHORIZED,
+                message: `(whitelistMiddleware) You denied the app or the token is invalid/expired.`,
+            };
+        }
+
+        next();
+    } catch (err: any) {
+        console.error(`(whitelistMiddleware) ${err.message}`);
+        return res.status(500).json({
+            status: 500,
+            message: err.message,
+        });
+    }
 };
