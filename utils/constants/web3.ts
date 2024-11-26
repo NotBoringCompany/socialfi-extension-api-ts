dfb1a979a2e00bec9c9a237223ac9430bcf914fe
Suwandre <suwandresukijat@gmail.com>
2024-11-26 13:36:01 +0100

    feat: added all nft/sft integration contracts instances into web3.ts utils

--- utils/constants/web3.ts
+++ utils/constants/web3.ts
@@ -11,6 +11,10 @@ export const BLAST_TESTNET_PROVIDER = new ethers.providers.JsonRpcProvider(`http
 // export const ETH_MAINNET_PROVIDER = new ethers.providers.JsonRpcProvider(`https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY!}`);
 export const ETH_MAINNET_PROVIDER = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY!}`);
 export const XPROTOCOL_TESTNET_PROVIDER = new ethers.providers.JsonRpcProvider(`https://rpc-xprotocol-testnet-kzg4iy2205.t.conduit.xyz/${process.env.X_PROTOCOL_TESTNET_API_KEY!}`);
+/**
+ * temporary public testnet provider for Kairos (kaia's testnet)
+ */
+export const KAIA_TESTNET_PROVIDER = new ethers.providers.JsonRpcProvider(`https://kaia-kairos.blockpi.network/v1/rpc/public`);
 
 /// TonWeb instance with API Key
 export const TON_WEB = new TonWeb(new TonWeb.HttpProvider('https://toncenter.com/api/v2/jsonRPC', { apiKey: process.env.TON_API_KEY} ));
@@ -28,15 +32,16 @@ export const DEPLOYER_WALLET = (provider: ethers.providers.JsonRpcProvider) => {
     return new ethers.Wallet(process.env.DEPLOYER_PRIVATE_KEY!, provider);
 }
 
-export const COOKIE_CONTRACT_ADDRESS = process.env.COOKIE_CONTRACT!;
-export const COOKIE_CONTRACT_DECIMALS = 8;
-
 export const KOS_CONTRACT_ADDRESS = process.env.KOS_CONTRACT!;
 export const KEYCHAIN_CONTRACT_ADDRESS = process.env.KEYCHAIN_CONTRACT!;
 export const SUPERIOR_KEYCHAIN_CONTRACT_ADDRESS = process.env.SUPERIOR_KEYCHAIN_CONTRACT!;
 
-// wonderbits address in XProtocol testnet
+// kaia's testnet contract addresses
 export const WONDERBITS_CONTRACT_ADDRESS = process.env.WONDERBITS_CONTRACT!;
+export const ISLANDS_CONTRACT_ADDRESS = process.env.ISLANDS_CONTRACT!;
+export const BIT_COSMETICS_CONTRACT_ADDRESS = process.env.BIT_COSMETICS_CONTRACT!;
+export const WONDERBITS_SFT_CONTRACT_ADDRESS = process.env.WONDERBITS_SFT_CONTRACT!;
+export const CUSTODIAL_CONTRACT_ADDRESS = process.env.CUSTODIAL_CONTRACT!;
 
 export const KOS_ARTIFACT = JSON.parse(
     fs.readFileSync(
@@ -53,6 +58,36 @@ export const KEYCHAIN_ARTIFACT = JSON.parse(
 export const SUPERIOR_KEYCHAIN_ARTIFACT = JSON.parse(
     fs.readFileSync(
         path.join(__dirname, '../../artifacts/SuperiorKeychain.json')
+    ).toString()
+);
+
+export const WONDERBITS_ARTIFACT = JSON.parse(
+    fs.readFileSync(
+        path.join(__dirname, '../../artifacts/Wonderbits.json')
+    ).toString()
+);
+
+export const ISLANDS_ARTIFACT = JSON.parse(
+    fs.readFileSync(
+        path.join(__dirname, '../../artifacts/Islands.json')
+    ).toString()
+);
+
+export const BIT_COSMETICS_ARTIFACT = JSON.parse(
+    fs.readFileSync(
+        path.join(__dirname, '../../artifacts/BitCosmetics.json')
+    ).toString()
+);
+
+export const WONDERBITS_SFT_ARTIFACT = JSON.parse(
+    fs.readFileSync(
+        path.join(__dirname, '../../artifacts/WonderbitsSFT.json')
+    ).toString()
+);
+
+export const CUSTODIAL_CONTRACT_ARTIFACT = JSON.parse(
+    fs.readFileSync(
+        path.join(__dirname, '../../artifacts/Custodial.json')
     ).toString()
 );
 
@@ -80,6 +115,51 @@ export const KEYCHAIN_CONTRACT = new ethers.Contract(
 export const SUPERIOR_KEYCHAIN_CONTRACT = new ethers.Contract(
     SUPERIOR_KEYCHAIN_CONTRACT_ADDRESS,
     SUPERIOR_KEYCHAIN_ARTIFACT.abi,
+    DEPLOYER_WALLET(ETH_MAINNET_PROVIDER)
+);
+
+/**
+ * The wonderbits contract instance (using admin wallet)
+ */
+export const WONDERBITS_CONTRACT = new ethers.Contract(
+    WONDERBITS_CONTRACT_ADDRESS,
+    WONDERBITS_ARTIFACT.abi,
+    DEPLOYER_WALLET(ETH_MAINNET_PROVIDER)
+);
+
+/**
+ * The islands contract instance (using admin wallet)
+ */
+export const ISLANDS_CONTRACT = new ethers.Contract(
+    ISLANDS_CONTRACT_ADDRESS,
+    ISLANDS_ARTIFACT.abi,
+    DEPLOYER_WALLET(ETH_MAINNET_PROVIDER)
+);
+
+/**
+ * The bit cosmetics contract instance (using admin wallet)
+ */
+export const BIT_COSMETICS_CONTRACT = new ethers.Contract(
+    BIT_COSMETICS_CONTRACT_ADDRESS,
+    BIT_COSMETICS_ARTIFACT.abi,
+    DEPLOYER_WALLET(ETH_MAINNET_PROVIDER)
+);
+
+/**
+ * The wonderbits SFT contract instance (using admin wallet)
+ */
+export const WONDERBITS_SFT_CONTRACT = new ethers.Contract(
+    WONDERBITS_SFT_CONTRACT_ADDRESS,
+    WONDERBITS_SFT_ARTIFACT.abi,
+    DEPLOYER_WALLET(ETH_MAINNET_PROVIDER)
+);
+
+/**
+ * The custodial contract instance (using admin wallet)
+ */
+export const CUSTODIAL_CONTRACT = new ethers.Contract(
+    CUSTODIAL_CONTRACT_ADDRESS,
+    CUSTODIAL_CONTRACT_ARTIFACT.abi,
     DEPLOYER_WALLET(ETH_MAINNET_PROVIDER)
 );
 
@@ -118,6 +198,71 @@ export const SUPERIOR_KEYCHAIN_CONTRACT_USER = (privateKey: string) => {
     return new ethers.Contract(
         SUPERIOR_KEYCHAIN_CONTRACT_ADDRESS,
         SUPERIOR_KEYCHAIN_ARTIFACT.abi,
+        wallet
+    );
+}
+
+/**
+ * The wonderbits contract instance (using user wallet, requires their private key)
+ */
+export const WONDERBITS_CONTRACT_USER = (privateKey: string) => {
+    const wallet = new ethers.Wallet(privateKey, ETH_MAINNET_PROVIDER);
+
+    return new ethers.Contract(
+        WONDERBITS_CONTRACT_ADDRESS,
+        WONDERBITS_ARTIFACT.abi,
+        wallet
+    );
+}
+
+/**
+ * The islands contract instance (using user wallet, requires their private key)
+ */
+export const ISLANDS_CONTRACT_USER = (privateKey: string) => {
+    const wallet = new ethers.Wallet(privateKey, ETH_MAINNET_PROVIDER);
+
+    return new ethers.Contract(
+        ISLANDS_CONTRACT_ADDRESS,
+        ISLANDS_ARTIFACT.abi,
+        wallet
+    );
+}
+
+/**
+ * The bit cosmetics contract instance (using user wallet, requires their private key)
+ */
+export const BIT_COSMETICS_CONTRACT_USER = (privateKey: string) => {
+    const wallet = new ethers.Wallet(privateKey, ETH_MAINNET_PROVIDER);
+
+    return new ethers.Contract(
+        BIT_COSMETICS_CONTRACT_ADDRESS,
+        BIT_COSMETICS_ARTIFACT.abi,
+        wallet
+    );
+}
+
+/**
+ * The wonderbits SFT contract instance (using user wallet, requires their private key)
+ */
+export const WONDERBITS_SFT_CONTRACT_USER = (privateKey: string) => {
+    const wallet = new ethers.Wallet(privateKey, ETH_MAINNET_PROVIDER);
+
+    return new ethers.Contract(
+        WONDERBITS_SFT_CONTRACT_ADDRESS,
+        WONDERBITS_SFT_ARTIFACT.abi,
+        wallet
+    );
+}
+
+/**
+ * The custodial contract instance (using user wallet, requires their private key)
+ */
+export const CUSTODIAL_CONTRACT_USER = (privateKey: string) => {
+    const wallet = new ethers.Wallet(privateKey, ETH_MAINNET_PROVIDER);
+
+    return new ethers.Contract(
+        CUSTODIAL_CONTRACT_ADDRESS,
+        CUSTODIAL_CONTRACT_ARTIFACT.abi,
         wallet
     );
 }
